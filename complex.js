4448441630d1741296fdb2de868f4c9d16f723c4
Uvais Singdi <119435947+ussingdi@users.noreply.github.com>
2024-11-23 00:00:01 -0600

    Update complex.js

--- complex.js
+++ complex.js
@@ -28,23 +28,4 @@ function checkPrime(num) {
     return true;
 }
 
-function findMaximum(numbers) {
-    // Find the maximum number in a list.
-    if (!numbers.length) return null;
-    return Math.max(...numbers);
-}
 
-function readFile(filePath) {
-    // Read the content of a file.
-    const fs = require("fs");
-    return fs.readFileSync(filePath, "utf8");
-}
-
-module.exports = {
-    calculateSum,
-    reverseString,
-    generateFibonacci,
-    checkPrime,
-    findMaximum,
-    readFile
-};
